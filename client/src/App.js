8364ee622eda42de1ded28816b351409558c533c
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2022-05-06 20:02:33 +0530

    Added new gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerUser (e) {
+    e.preventDefault();    
+    const response = await fetch("http://localhost:1111/api/register", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      })
+    })
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder='Username'></input>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Email'></input>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" placeholder='Password'></input>
+        <input type="submit" value="Register"></input>
+      </form>
     </div>
   );
 }
